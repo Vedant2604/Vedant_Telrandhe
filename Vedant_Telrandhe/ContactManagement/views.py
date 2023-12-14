fac29bbe1b6c0d7a9daab77606506d85fb8e8c02
Vedant2604 <vedantm264@gmail.com>
2023-12-13 21:30:26 -0800

    Implemented AddContact Functionality

--- Vedant_Telrandhe/ContactManagement/views.py
+++ Vedant_Telrandhe/ContactManagement/views.py
@@ -9,3 +9,19 @@ def index(request):
     contacts=Contact.objects.all()
 
     return render(request,'index.html',{'contacts':contacts})
+
+def addContact(request, id=None):
+    if id:
+        contact = Contact.objects.get(pk=id)
+        form = ContactForm(request.POST or None, instance=contact)
+    else:
+        form = ContactForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('index')  
+            except ValidationError as e:
+                form.add_error(None, e)
+    return render(request, 'addContact.html', {'form': form})
